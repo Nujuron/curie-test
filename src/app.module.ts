6d30c3d78ae144df1fce663350fa5a9c965227c8
Nujuron <adri.brito@outlook.es>
2021-09-30 10:06:31 +0100

    TypeORM and graphql config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TasksModule } from './tasks/tasks.module';
+// typeorm api
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TasksORMModule } from './tasks-api/task.module';
+import { withCache } from './orm.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot(withCache),
+    TasksORMModule,
+    TasksModule,
+    GraphQLModule.forRoot({
+      installSubscriptionHandlers: true,
+      autoSchemaFile: 'schema.gql',
+    }),
+  ],
 })
 export class AppModule {}
