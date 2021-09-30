4cee06a5053d044214457e2e9c04902d3f3c3eb2
Nujuron <adri.brito@outlook.es>
2021-09-30 19:02:20 +0100

    Resolver fixes

--- src/tasks/tasks.resolver.ts
+++ src/tasks/tasks.resolver.ts
@@ -1,21 +1,20 @@
-import { Resolver, Query, Mutation, Args, Int, ID } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, ID, Subscription } from '@nestjs/graphql';
+import { PubSub } from 'graphql-subscriptions';
 import { TasksService } from './tasks.service';
 import { Task } from './entities/task.entity';
 import { CreateTaskInput } from './dto/create-task.input';
 import { UpdateTaskInput } from './dto/update-task.input';
+import { TasksArgs } from './dto/tasks.args';
+
+const pubSub = new PubSub();
 
 @Resolver(() => Task)
 export class TasksResolver {
   constructor(private readonly tasksService: TasksService) {}
 
-  @Mutation(() => Task)
-  createTask(@Args('createTaskInput') createTaskInput: CreateTaskInput) {
-    return this.tasksService.create(createTaskInput);
-  }
-
   @Query(() => [Task], { name: 'tasks' })
-  findAll() {
-    return this.tasksService.findAll();
+  findAll(@Args() tasksArgs: TasksArgs): Promise<Task[]> {
+    return this.tasksService.findAll(tasksArgs);
   }
 
   @Query(() => Task, { name: 'task' })
@@ -29,7 +28,21 @@ export class TasksResolver {
   }
 
   @Mutation(() => Task)
-  removeTask(@Args('id', { type: () => ID }) id: number) {
+  async createTask(
+    @Args('createTaskInput') createTaskInput: CreateTaskInput
+  ): Promise<Task> {
+    const task = await this.tasksService.create(createTaskInput);
+    pubSub.publish('taskAdded', { taskAdded: task });
+    return task;
+  }
+
+  @Mutation(() => Task)
+  async removeTask(@Args('id', { type: () => ID }) id: number) {
     return this.tasksService.remove(id);
+  }
+
+  @Subscription(returns => Task)
+  taskAdded() {
+    return pubSub.asyncIterator('taskAdded');
   }
 }
